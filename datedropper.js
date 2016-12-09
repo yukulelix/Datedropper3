8c0b3baded405346dcea0f3070d7f420740ef58f
Felice Gattuso <me@felicegattuso.com>
2016-12-09 12:38:41 +0100

    Added Vietnamese translation

--- datedropper.js
+++ datedropper.js
@@ -1028,6 +1028,60 @@
 					]
 				}
 			},
+			'vi':{
+				name:'Tiếng việt',
+				gregorian:false,
+				months:{
+					short:[
+						'Th.01',
+						'Th.02',
+						'Th.03',
+						'Th.04',
+						'Th.05',
+						'Th.06',
+						'Th.07',
+						'Th.08',
+						'Th.09',
+						'Th.10',
+						'Th.11',
+						'Th.12'
+					],
+					full:[
+						'Tháng 01',
+						'Tháng 02',
+						'Tháng 03',
+						'Tháng 04',
+						'Tháng 05',
+						'Tháng 06',
+						'Tháng 07',
+						'Tháng 08',
+						'Tháng 09',
+						'Tháng 10',
+						'Tháng 11',
+						'Tháng 12'
+					]
+				},
+				weekdays:{
+					short:[
+						'CN',
+						'T2',
+						'T3',
+						'T4',
+						'T5',
+						'T6',
+						'T7'
+					],
+					full:[
+						'Chủ nhật',
+						'Thứ hai',
+						'Thứ ba',
+						'Thứ tư',
+						'Thứ năm',
+						'Thứ sáu',
+						'Thứ bảy'
+					]
+				}
+			}
 		},
 		
 		// MAIN VARS
